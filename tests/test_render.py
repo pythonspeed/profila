5eab634e71a721db38f40e256461111a717aab77
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-01-29 19:17:28 -0500

    Tests for rendering.

---
+++ tests/test_render.py
@@ -0,0 +1,19 @@
+"""
+Tests for ``profila._render``.
+"""
+
+from profila._stats import FinalStats
+from profila._render import render_text
+
+
+def test_render_text(snapshot):
+    """
+    Minimal test for ``render_text()``.
+    """
+    final_stats = FinalStats(
+        total_samples=1000,
+        percent_bad_samples=9.9,
+        percent_other_samples=15.1,
+        numba_samples={"scripts_for_tests/simple.py": {12: 35.0, 15: 40.0}},
+    )
+    assert render_text(final_stats) == snapshot
