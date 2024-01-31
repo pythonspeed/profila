49ceda4425042cd5e93af0053ecdda0b3308709d
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-01-31 15:43:41 -0500

    First pass removing need for sudo.

--- src/profila/_ipython.py
+++ src/profila/_ipython.py
@@ -1,6 +1,7 @@
 """
 IPython/Jupyter magics.
 """
+import ctypes
 import json
 import sys
 import os
@@ -12,6 +13,14 @@ from ._render import render_text
 from IPython.core.magic import Magics, magics_class, cell_magic
 from IPython.core.display import display, Markdown
 
+libc = ctypes.CDLL("libc.so.6")
+prctl = libc.prctl
+prctl.argtypes = [ctypes.c_int]
+prctl.restype = ctypes.c_int
+# From linux/prctl.h:
+PR_SET_PTRACER = ctypes.c_int(0x59616D61)
+PR_SET_PTRACER_ANY = ctypes.c_long(-1)
+
 
 @magics_class
 class ProfilaMagics(Magics):
@@ -19,10 +28,9 @@ class ProfilaMagics(Magics):
     def profila(self, line, cell):
         """Run the cell under a profiler."""
         del line
-
+        prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY)
         profiler = Popen(
             [
-                "pkexec",
                 sys.executable,
                 "-m",
                 "profila",
@@ -38,6 +46,7 @@ class ProfilaMagics(Magics):
         assert json.loads(profiler.stdout.readline().rstrip())["message"] == "attached"
         self.shell.run_cell(cell)
         profiler.stdin.close()
+        prctl(PR_SET_PTRACER, ctypes.c_long(0))
         message = json.loads(profiler.stdout.readline().rstrip())
         for line_mappings in message["stats"]["numba_samples"].values():
             for line, pct in list(line_mappings.items()):
